cc1f20c6c097bf5978dc0db51bb23503f207cf94
jamesasmith83 <james.a.smith.83@gmail.com>
2017-03-11 17:48:24 +0000

    services and pipes

---
+++ app/shared/services/data.service.ts
@@ -0,0 +1,9 @@
+import {Injectable } from '@angular/core';
+import {Http, Response, Headers } from '@angular/http';
+//Grab everything with import 'rxjs/Rx';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
